18d1c40c8c056850fdee5da2f54246a91cb50457
israel-bsi <masterisraelsilva@gmail.com>
2025-06-28 16:53:55 -0300

    Finalizado testes na development

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
+using AboutMe;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using AboutMe;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 
