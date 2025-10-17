498b9f98537f38e7127f663eb95310344f61c022
israel-bsi <masterisraelsilva@gmail.com>
2025-10-17 13:27:35 -0300

    adiciona chave seletora para mudar idioma, adiciona icones das tecnologias

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using AboutMe;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using System.Globalization;
+using Microsoft.JSInterop;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,4 +12,34 @@ builder.Services.AddLocalization(options => options.ResourcesPath = "Resources")
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+
+var js = host.Services.GetRequiredService<IJSRuntime>();
+var cultureString = await js.InvokeAsync<string>("localStorage.getItem", "culture");
+
+CultureInfo culture;
+
+if (!string.IsNullOrEmpty(cultureString))
+{
+    culture = new CultureInfo(cultureString);
+}
+else
+{
+    var browserLanguage = await js.InvokeAsync<string>("eval", "navigator.language || navigator.userLanguage");
+    
+    if (!string.IsNullOrEmpty(browserLanguage) && browserLanguage.StartsWith("en", StringComparison.OrdinalIgnoreCase))
+    {
+        culture = new CultureInfo("en-US");
+    }
+    else
+    {
+        culture = new CultureInfo("pt-BR");
+    }
+    
+    await js.InvokeVoidAsync("localStorage.setItem", "culture", culture.Name);
+}
+
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+await host.RunAsync();
